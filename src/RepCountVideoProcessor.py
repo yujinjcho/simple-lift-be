26ce569c8816736a6e135090dcdc315e427111f4
yujin <yujinjcho@gmail.com>
2023-04-01 14:31:22 -0700

    clean up

--- src/RepCountVideoProcessor.py
+++ src/RepCountVideoProcessor.py
@@ -7,101 +7,53 @@ mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 mp_pose = mp.solutions.pose
 
+bone_connections = [
+    (mp_holistic.PoseLandmark.LEFT_KNEE.value, mp_holistic.PoseLandmark.LEFT_HIP.value),
+    (mp_holistic.PoseLandmark.RIGHT_KNEE.value, mp_holistic.PoseLandmark.RIGHT_HIP.value),
+    (mp_holistic.PoseLandmark.LEFT_ANKLE.value, mp_holistic.PoseLandmark.LEFT_KNEE.value),
+    (mp_holistic.PoseLandmark.RIGHT_ANKLE.value, mp_holistic.PoseLandmark.RIGHT_KNEE.value),
+    (mp_holistic.PoseLandmark.LEFT_HIP.value, mp_holistic.PoseLandmark.RIGHT_HIP.value),
+    (mp_holistic.PoseLandmark.LEFT_HIP.value, mp_holistic.PoseLandmark.LEFT_SHOULDER.value),
+    (mp_holistic.PoseLandmark.RIGHT_HIP.value, mp_holistic.PoseLandmark.RIGHT_SHOULDER.value),
+    (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_SHOULDER.value),
+    (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.LEFT_ELBOW.value),
+    (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.LEFT_EAR.value),
+    (mp_holistic.PoseLandmark.LEFT_ELBOW.value, mp_holistic.PoseLandmark.LEFT_WRIST.value),
+    (mp_holistic.PoseLandmark.RIGHT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_ELBOW.value),
+    (mp_holistic.PoseLandmark.RIGHT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_EAR.value),
+    (mp_holistic.PoseLandmark.LEFT_EAR.value, mp_holistic.PoseLandmark.RIGHT_EAR.value),
+    (mp_holistic.PoseLandmark.RIGHT_ELBOW.value, mp_holistic.PoseLandmark.RIGHT_WRIST.value),
+]
+
 
 class RepCountVideoProcessor:
 
     def __init__(self):
         self.rep_state = 'pending'
         self.rep_count = 0
-        self.highest_confidence_ankles = None
+        self.highest_confidence_heels = None
         self.max_confidence = 0
+        self.angle_threshold = 100
+
+        # video outlining
+        self.line_color = (0, 255, 0)
+        self.line_thickness = 2
 
     def draw(self, frame, landmarks, pose_landmarks):
-        bone_connections = [
-            (mp_holistic.PoseLandmark.LEFT_KNEE.value, mp_holistic.PoseLandmark.LEFT_HIP.value),
-            (mp_holistic.PoseLandmark.RIGHT_KNEE.value, mp_holistic.PoseLandmark.RIGHT_HIP.value),
-            (mp_holistic.PoseLandmark.LEFT_HIP.value, mp_holistic.PoseLandmark.RIGHT_HIP.value),
-            (mp_holistic.PoseLandmark.LEFT_HIP.value, mp_holistic.PoseLandmark.LEFT_SHOULDER.value),
-            (mp_holistic.PoseLandmark.RIGHT_HIP.value, mp_holistic.PoseLandmark.RIGHT_SHOULDER.value),
-            (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_SHOULDER.value),
-            (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.LEFT_ELBOW.value),
-            (mp_holistic.PoseLandmark.LEFT_SHOULDER.value, mp_holistic.PoseLandmark.LEFT_EAR.value),
-            (mp_holistic.PoseLandmark.LEFT_ELBOW.value, mp_holistic.PoseLandmark.LEFT_WRIST.value),
-            (mp_holistic.PoseLandmark.RIGHT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_ELBOW.value),
-            (mp_holistic.PoseLandmark.RIGHT_SHOULDER.value, mp_holistic.PoseLandmark.RIGHT_EAR.value),
-            (mp_holistic.PoseLandmark.LEFT_EAR.value, mp_holistic.PoseLandmark.RIGHT_EAR.value),
-            (mp_holistic.PoseLandmark.RIGHT_ELBOW.value, mp_holistic.PoseLandmark.RIGHT_WRIST.value),
-        ]
+        self.handle_stored_foot_position(frame, landmarks, pose_landmarks)
 
-        # use estimated feet if none currently tracked
-        if self.highest_confidence_ankles is None:
-            bone_connections.append(
-                (mp_holistic.PoseLandmark.LEFT_ANKLE.value, mp_holistic.PoseLandmark.LEFT_KNEE.value))
-            bone_connections.append(
-                (mp_holistic.PoseLandmark.RIGHT_ANKLE.value, mp_holistic.PoseLandmark.RIGHT_KNEE.value))
-
-        # use stored feet position if tracked
-        if self.highest_confidence_ankles is not None:
-            left_ankle, right_ankle = self.highest_confidence_ankles
-            landmarks[mp_holistic.PoseLandmark.LEFT_HEEL.value] = left_ankle
-            landmarks[mp_holistic.PoseLandmark.RIGHT_HEEL.value] = right_ankle
-
-            cv2.circle(frame, left_ankle, 5, (0, 0, 255), -1)
-            cv2.circle(frame, right_ankle, 5, (0, 0, 255), -1)
-
-        if self.rep_state == 'up':
-            left_ankle_idx = mp_holistic.PoseLandmark.LEFT_HEEL.value
-            right_ankle_idx = mp_holistic.PoseLandmark.RIGHT_HEEL.value
-            left_ankle_confidence = pose_landmarks.landmark[left_ankle_idx].visibility
-            right_ankle_confidence = pose_landmarks.landmark[right_ankle_idx].visibility
-            average_confidence = (left_ankle_confidence + right_ankle_confidence) / 2
-
-            if self.highest_confidence_ankles is None or average_confidence > self.max_confidence:
-                left_ankle = landmarks[left_ankle_idx]
-                right_ankle = landmarks[right_ankle_idx]
-                self.highest_confidence_ankles = (left_ankle, right_ankle)
-                self.max_confidence = average_confidence
-
-        line_color = (0, 255, 0)
-        line_thickness = 2
-
+        # Draw lines
         for connection in bone_connections:
             start, end = connection
-            cv2.line(frame, landmarks[start], landmarks[end], line_color, line_thickness)
 
-        # Check if the first "up" state is reached
-        first_up_state_reached = self.rep_count > 0
+            # don't draw if we're using stored ones
+            is_ankle = start in [mp_holistic.PoseLandmark.LEFT_ANKLE.value, mp_holistic.PoseLandmark.RIGHT_ANKLE.value]
+            if self.highest_confidence_heels is not None and is_ankle:
+                continue
 
-        if self.rep_state == "up" or first_up_state_reached:
-            # Draw the connections between knee and ankle only after the first "up" state is reached
-            left_ankle = landmarks[mp_holistic.PoseLandmark.LEFT_HEEL.value]
-            left_knee = landmarks[mp_holistic.PoseLandmark.LEFT_KNEE.value]
-            right_ankle = landmarks[mp_holistic.PoseLandmark.RIGHT_HEEL.value]
-            right_knee = landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value]
+            cv2.line(frame, landmarks[start], landmarks[end], self.line_color, self.line_thickness)
 
-            cv2.line(frame, left_knee, left_ankle, line_color, line_thickness)
-            cv2.line(frame, right_knee, right_ankle, line_color, line_thickness)
-
-        left_shoulder = landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value]
-        left_hip = landmarks[mp_holistic.PoseLandmark.LEFT_HIP.value]
-        left_knee = landmarks[mp_holistic.PoseLandmark.LEFT_KNEE.value]
-
-        right_shoulder = landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value]
-        right_hip = landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value]
-        right_knee = landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value]
-
-        left_angle = angle_between_points(left_shoulder, left_hip, left_knee)
-        right_angle = angle_between_points(right_shoulder, right_hip, right_knee)
-
-        angle_threshold = 100
-
-        if self.rep_state == 'pending' and left_angle < angle_threshold and right_angle < angle_threshold:
-            self.rep_state = 'start'
-        elif self.rep_state == 'start' and left_angle > angle_threshold and right_angle > angle_threshold:
-            self.rep_state = 'up'
-        elif self.rep_state == 'up' and left_angle < angle_threshold and right_angle < angle_threshold:
-            self.rep_state = 'start'
-            self.rep_count += 1
+        left_angle, right_angle = self.update_rep_state(landmarks)
 
         rep_text = f"Rep: {self.rep_count}"
         state_text = f"State: {self.rep_state}"
@@ -127,6 +79,65 @@ class RepCountVideoProcessor:
         cv2.putText(frame, rep_text, (10, rep_text_y), font, font_scale, text_color, text_thickness, line_type)
         cv2.putText(frame, state_text, (10, state_text_y), font, font_scale, text_color, text_thickness, line_type)
         cv2.putText(frame, left_angle, (10, left_angle_text_y), font, font_scale, text_color, text_thickness, line_type)
-        cv2.putText(frame, right_angle, (10, right_angle_text_y), font, font_scale, text_color, text_thickness, line_type)
+        cv2.putText(frame, right_angle, (10, right_angle_text_y), font, font_scale, text_color, text_thickness,
+                    line_type)
 
+    def update_rep_state(self, landmarks):
+        left_shoulder = landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value]
+        left_hip = landmarks[mp_holistic.PoseLandmark.LEFT_HIP.value]
+        left_knee = landmarks[mp_holistic.PoseLandmark.LEFT_KNEE.value]
 
+        right_shoulder = landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value]
+        right_hip = landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value]
+        right_knee = landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value]
+
+        left_angle = angle_between_points(left_shoulder, left_hip, left_knee)
+        right_angle = angle_between_points(right_shoulder, right_hip, right_knee)
+
+        angle_threshold = self.angle_threshold
+        if self.rep_state == 'pending' and left_angle < angle_threshold and right_angle < angle_threshold:
+            self.rep_state = 'start'
+        elif self.rep_state == 'start' and left_angle > angle_threshold and right_angle > angle_threshold:
+            self.rep_state = 'up'
+        elif self.rep_state == 'up' and left_angle < angle_threshold and right_angle < angle_threshold:
+            self.rep_state = 'start'
+            self.rep_count += 1
+
+        return left_angle, right_angle
+
+    def handle_stored_foot_position(self, frame, landmarks, pose_landmarks):
+        # start tracking foot position when 'up' state
+        if self.rep_state == 'up':
+            left_heel_idx = mp_holistic.PoseLandmark.LEFT_HEEL.value
+            right_heel_idx = mp_holistic.PoseLandmark.RIGHT_HEEL.value
+            left_heel_confidence = pose_landmarks.landmark[left_heel_idx].visibility
+            right_heel_confidence = pose_landmarks.landmark[right_heel_idx].visibility
+            average_confidence = (left_heel_confidence + right_heel_confidence) / 2
+
+            # we're either not tracking or we have a higher confidence interval
+            if self.highest_confidence_heels is None or average_confidence > self.max_confidence:
+                left_heel = landmarks[left_heel_idx]
+                right_heel = landmarks[right_heel_idx]
+                self.highest_confidence_heels = (left_heel, right_heel)
+                self.max_confidence = average_confidence
+
+        # nothing else if we're not tracking currently
+        if self.highest_confidence_heels is None:
+            return
+
+        left_ankle, right_ankle = self.highest_confidence_heels
+        landmarks[mp_holistic.PoseLandmark.LEFT_HEEL.value] = left_ankle
+        landmarks[mp_holistic.PoseLandmark.RIGHT_HEEL.value] = right_ankle
+
+        # draw after first up is reached or after we've performed a rep
+        if self.rep_state == "up" or self.rep_count > 0:
+            left_ankle = landmarks[mp_holistic.PoseLandmark.LEFT_HEEL.value]
+            left_knee = landmarks[mp_holistic.PoseLandmark.LEFT_KNEE.value]
+            right_ankle = landmarks[mp_holistic.PoseLandmark.RIGHT_HEEL.value]
+            right_knee = landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value]
+
+            cv2.line(frame, left_knee, left_ankle, self.line_color, self.line_thickness)
+            cv2.line(frame, right_knee, right_ankle, self.line_color, self.line_thickness)
+
+            cv2.circle(frame, left_ankle, 5, (0, 0, 255), -1)
+            cv2.circle(frame, right_ankle, 5, (0, 0, 255), -1)
